e745834b7ac3b6da2c278127f32e73e8d88a72df
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-02-17 22:44:31 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/About" element={<About />} />
+          <Route exact path="/Con " element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,41 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav
-        class="navbar navbar-expand-lg navbar-light bg-light"
-        style={{ backgroundColor: "#e3f2fd" }}
-      >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Navbar
+          </a>
           <button
             class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div
-            class="collapse navbar-collapse d-flex justify-content-end"
-            id="navbarTogglerDemo01"
-          >
-            <a class="navbar-brand" href="/">
-              iNoteBook
-            </a>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end ">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
+                <Link to="/" class="nav-link active" aria-current="page">
                   Home
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/">
+                <Link to="/About" class="nav-link">
                   About
-                </a>
+                </Link>
               </li>
+
               <li class="nav-item">
-                <a class="nav-link" href="/">
+                <Link class="nav-link" to="/Con">
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
